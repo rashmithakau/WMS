cb2f46743d6a46ea01d30d4e9cbf243736138f19
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-07-09 10:54:59 +0530

    chore:tailwind-config

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,20 +1,33 @@
-import mongoose from "mongoose";
 import User from "../models/user.model.js";
+import { userValidationSchema } from '../validation/userValidation.js';
 
-export const createUser = async (req,res) => {
-    const user = req.body;
+export const createUser = async (req, res) => {
+  const user = req.body;
 
-    if(!user.userName || !user.password || !user.phoNumber){
-        return res.status(400).json({message: "Please fill all the fields"});
-    }
+  // Validate request body
+  const { error } = userValidationSchema.validate(user,{ abortEarly: false });
 
-    const newUser = new User(user);
+  if (error) {
+    return res.status(400).json({
+      message: 'Validation failed',
+      errors: error.details.map(err => err.message), // array of all error messages
+    });
+  }
+  
 
-    try {
-        await newUser.save();
-        res.status(201).json({success: true, message: "User created successfully", user: newUser});
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json({success: false, message: "Internal server error"});
-    }
-}
+  const newUser = new User(user);
+
+  try {
+    await newUser.save();
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "User created successfully",
+        user: newUser,
+      });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
